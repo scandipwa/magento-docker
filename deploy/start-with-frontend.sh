b019d5c5f5669f3f508aabcbb77c5b508a86274f
negzu <yegor.batov@gmail.com>
2020-02-18 20:16:52 +0200

    Mac optimizations - Add volume core-frontend-data for core frontend container - Implement mutagen configuration for both core and frontend dc configs - FIx node container not having wget - Keep maildev configuration in local setup only

--- deploy/start-with-frontend.sh
+++ deploy/start-with-frontend.sh
@@ -62,11 +62,7 @@ function pwa_theme_install {
   magento scandipwa:theme:bootstrap Scandiweb/pwa -n || true
   php bin/magento setup:upgrade
 
-  echo "${blue}${bold}Building PWA theme${normal}"
-  cd $BASEPATH/app/design/frontend/Scandiweb/pwa
-  npm ci
-  npm run build
-  cd $BASEPATH
+  echo "${blue}${bold}Frontend container will build PWA theme${normal}"
 }
 
 # Empty the redis config caches to avoid errors with module configuration
