54ecd38a163c53681f284ca94fefb1f41d098b1b
negzu <yegor.batov@gmail.com>
2020-02-19 14:17:01 +0200

    Optimize frontend deploy script acquiring process Make them be mounted into frontend container on deploy instead of copied on build

--- build/frontend/start-core.sh
+++ build/frontend/start-core.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "Waiting for Mutagen to sync initial files"
+echo "Waiting for initial files to be synced"
 while ! [ -f ./package.json -a -f ./package-lock.json ]
 do
   sleep 2
 done
 
+echo "Installing node modules"
 npm ci
 npm install pm2 forever -g
 pm2-runtime process-core.yml
