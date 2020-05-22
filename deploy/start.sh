a2e59f6f4b6b3eeee51d1f379298d964dc9ca555
Alfreds Genkins <alfreds@scandiweb.com>
2020-05-22 13:02:18 +0300

    Possibly fixed session issue and update deps

--- deploy/start.sh
+++ deploy/start.sh
@@ -206,6 +206,7 @@ function magento_redis_config {
       --session-save=redis \
       --session-save-redis-host=redis \
       --session-save-redis-log-level=3 \
+      --session-save-redis-max-concurrency=30 \
   --session-save-redis-db=1
   # Elasticsearch5 as a search engine
   echo "${blue}${bold}Setting Elasticsearch5 as a search engine${normal}"
