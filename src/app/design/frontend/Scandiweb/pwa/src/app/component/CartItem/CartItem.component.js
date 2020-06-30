4a30c71ba9ad7593492a16302659dd24be9399e0
Rudolfs Torsters <torsters.rudolfs@gmail.com>
2020-06-30 13:07:46 +0300

    Swipe delete cart item on mobile devices

---
+++ src/app/design/frontend/Scandiweb/pwa/src/app/component/CartItem/CartItem.component.js
@@ -0,0 +1,271 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'Component/Link';
+import Image from 'Component/Image';
+import Field from 'Component/Field';
+import CartItemPrice from 'Component/CartItemPrice';
+import Loader from 'Component/Loader';
+import { CartItemType } from 'Type/MiniCart';
+import './CartItem.style';
+import Swiper from 'Component/CartItem/Swiper';
+
+
+/**
+ * Cart and CartOverlay item
+ * @class CartItem
+ */
+export default class CartItem extends PureComponent {
+    static propTypes = {
+        isLoading: PropTypes.bool.isRequired,
+        item: CartItemType.isRequired,
+        currency_code: PropTypes.string.isRequired,
+        isEditing: PropTypes.bool,
+        isLikeTable: PropTypes.bool,
+        handleRemoveItem: PropTypes.func.isRequired,
+        minSaleQuantity: PropTypes.number.isRequired,
+        maxSaleQuantity: PropTypes.number.isRequired,
+        handleChangeQuantity: PropTypes.func.isRequired,
+        getCurrentProduct: PropTypes.func.isRequired,
+        linkTo: PropTypes.oneOfType([
+            PropTypes.shape({
+                pathname: PropTypes.string,
+                search: PropTypes.string
+            }),
+            PropTypes.string
+        ]).isRequired,
+        thumbnail: PropTypes.string.isRequired
+    };
+
+    static defaultProps = {
+        isEditing: false,
+        isLikeTable: false
+    };
+
+    renderConfiguration() {
+        const {
+            item: {
+                product: {
+                    configurable_options,
+                    variants
+                }
+            },
+            isLikeTable,
+            getCurrentProduct
+        } = this.props;
+
+        if (!variants || !configurable_options) {
+            return null;
+        }
+
+        const product = getCurrentProduct() || {};
+        const { attributes = [] } = product;
+
+        return (
+            <ul
+              block="CartItem"
+              elem="Options"
+              mods={ { isLikeTable } }
+            >
+                { Object.entries(attributes).map(([key, { attribute_code, attribute_value }]) => (
+                    Object.keys(configurable_options).includes(key) && (
+                        <li
+                          key={ attribute_code }
+                          aria-label={ attribute_code }
+                          block="CartItem"
+                          elem="Option"
+                        >
+                            { configurable_options[attribute_code].attribute_options[attribute_value].label }
+                        </li>
+                    ))) }
+            </ul>
+        );
+    }
+
+    renderContent() {
+        const { isLikeTable, linkTo } = this.props;
+
+        return (
+            <Link to={ linkTo } block="CartItem" elem="Link">
+                <figure block="CartItem" elem="Wrapper">
+                    { this.renderImage() }
+                    <figcaption
+                      block="CartItem"
+                      elem="Content"
+                      mods={ { isLikeTable } }
+                    >
+                        { this.renderProductDetails() }
+                    </figcaption>
+                </figure>
+            </Link>
+        );
+    }
+
+    renderCustomizableOptions(customizableOptions) {
+        if (!customizableOptions || !customizableOptions.length) {
+            return null;
+        }
+
+        return (
+            <div block="CartItem" elem="CustomizableOptionsWrapper">
+                { customizableOptions.map(({ label, values, id }) => (
+                    <div
+                      block="CartItem"
+                      elem="CustomizableOption"
+                      key={ id }
+                    >
+                        <div
+                          block="CartItem"
+                          elem="CustomizableOptionLabel"
+                          key={ `label-${ id }` }
+                        >
+                            { `${ label }:` }
+                        </div>
+                        <div block="CartItem" elem="CustomizableOptionValues">
+                            { values.map(({ label, value }) => (
+                                <div
+                                  block="CartItem"
+                                  elem="CustomizableOptionValue"
+                                  key={ label }
+                                >
+                                    { label || value }
+                                </div>
+                            )) }
+                        </div>
+                    </div>
+                )) }
+            </div>
+        );
+    }
+
+    renderProductDetails() {
+        const {
+            isLikeTable,
+            currency_code,
+            item: {
+                row_total,
+                product: {
+                    name
+                },
+                customizable_options
+            }
+        } = this.props;
+
+        return (
+            <>
+                <p
+                  block="CartItem"
+                  elem="Heading"
+                  itemProp="name"
+                >
+                    { name }
+                </p>
+                { this.renderCustomizableOptions(customizable_options) }
+                { this.renderConfiguration() }
+                <CartItemPrice
+                  row_total={ row_total }
+                  currency_code={ currency_code }
+                  mix={ {
+                      block: 'CartItem',
+                      elem: 'Price',
+                      mods: { isLikeTable }
+                  } }
+                />
+            </>
+        );
+    }
+
+    renderActions() {
+        const {
+            isEditing,
+            isLikeTable,
+            item: { qty },
+            minSaleQuantity,
+            maxSaleQuantity,
+            handleRemoveItem,
+            handleChangeQuantity
+        } = this.props;
+
+        return (
+            <div
+              block="CartItem"
+              elem="Actions"
+              mods={ { isEditing, isLikeTable } }
+            >
+                <button
+                  block="CartItem"
+                  id="RemoveItem"
+                  name="RemoveItem"
+                  elem="Delete"
+                  aria-label="Remove item from cart"
+                  onClick={ handleRemoveItem }
+                >
+                    <span>{ __('Delete') }</span>
+                </button>
+                <Field
+                  id="item_qty"
+                  name="item_qty"
+                  type="number"
+                  isControlled
+                  min={ minSaleQuantity }
+                  max={ maxSaleQuantity }
+                  mix={ { block: 'CartItem', elem: 'Qty' } }
+                  value={ qty }
+                  onChange={ handleChangeQuantity }
+                />
+            </div>
+        );
+    }
+
+    renderImage() {
+        const { item: { product: { name } }, thumbnail } = this.props;
+
+        return (
+            <>
+                <Image
+                  src={ thumbnail }
+                  mix={ {
+                      block: 'CartItem',
+                      elem: 'Picture'
+                  } }
+                  ratio="custom"
+                  alt={ `Product ${name} thumbnail.` }
+                />
+                <img
+                  style={ { display: 'none' } }
+                  alt={ name }
+                  src={ thumbnail }
+                  itemProp="image"
+                />
+            </>
+        );
+    }
+
+    render() {
+        const { isLoading, handleRemoveItem } = this.props;
+        return (
+            <li
+              block="CartItem"
+              itemScope
+              itemType="http://schema.org/Product"
+            >
+                <Swiper 
+                onSwipe={handleRemoveItem}>
+                    <Loader isLoading={ isLoading } />
+                    { this.renderContent() }
+                    { this.renderActions() }
+                </Swiper>
+            </li>
+        );
+    }
+}
---
+++ src/app/design/frontend/Scandiweb/pwa/src/app/component/CartItem/Swiper.js
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+import './CartItem.style';
+
+class SwipeableListItem extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.listElement = null;
+    this.wrapper = null;
+    this.background = null;
+
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.onDragStartMouse = this.onDragStartMouse.bind(this);
+    this.onDragStartTouch = this.onDragStartTouch.bind(this);
+    this.onDragEndMouse = this.onDragEndMouse.bind(this);
+    this.onDragEndTouch = this.onDragEndTouch.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
+    this.onSwiped = this.onSwiped.bind(this);
+  }
+
+  dragStartX = 0
+  left = 0
+  dragged = false
+
+  // DOM Refs
+  listElement
+  wrapper
+  background
+
+  onDragStartMouse(evt) {
+    this.onDragStart(evt.clientX);
+    window.addEventListener("mousemove", this.onMouseMove);
+  }
+
+  onDragStartTouch(evt) {
+    const touch = evt.targetTouches[0];
+    this.onDragStart(touch.clientX);
+    window.addEventListener("touchmove", this.onTouchMove);
+  }
+
+  onDragStart(clientX) {
+    this.dragged = true;
+    this.dragStartX = clientX;
+    this.listElement.className = "swiperListItem";
+
+    requestAnimationFrame(this.updatePosition);
+  }
+  onMouseMove(evt) {
+    const left = evt.clientX - this.dragStartX;
+    if (left < 0) {
+      this.left = left;
+    }
+  }
+
+  onTouchMove(evt) {
+    const touch = evt.targetTouches[0];
+    const left = touch.clientX - this.dragStartX;
+    if (left < 0) {
+      this.left = left;
+    }
+  }
+  onDragEndMouse(evt) {
+    window.removeEventListener("mousemove", this.onMouseMove);
+    this.onDragEnd();
+  }
+
+  onDragEndTouch(evt) {
+    window.removeEventListener("touchmove", this.onTouchMove);
+    this.onDragEnd();
+  }
+
+  onDragEnd() {
+    if (this.dragged) {
+      this.dragged = false;
+
+      const threshold = this.props.threshold || 0.3;
+
+      if (this.left < this.listElement.offsetWidth * threshold * -1) {
+        this.left = -this.listElement.offsetWidth * 2;
+        this.onSwiped();
+      } else {
+        this.left = 0;
+      }
+      this.listElement.className = "swiperBouncingListItem";
+
+      this.listElement.style.transform = `translateX(${this.left}px)`;
+    }
+  }
+  onSwiped() {
+    if (this.props.onSwipe) {
+      this.props.onSwipe();
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("mouseup", this.onDragEndMouse);
+    window.addEventListener("touchend", this.onDragEndTouch);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("mouseup", this.onDragEndMouse);
+    window.removeEventListener("touchend", this.onDragEndTouch);
+  }
+  updatePosition() {
+    if (this.dragged) requestAnimationFrame(this.updatePosition);
+
+    const now = Date.now();
+    const elapsed = now - this.startTime;
+
+
+    if (this.dragged && elapsed > this.fpsInterval) {
+      this.listElement.style.transform = `translateX(${this.left}px)`;
+
+      const opacity = (Math.abs(this.left) / 100).toFixed(2);
+      if (opacity < 1 && opacity.toString() !== this.background.style.opacity) {
+        this.background.style.opacity = opacity.toString();
+      }
+      if (opacity >= 1) {
+        this.background.style.opacity = "1";
+      }
+
+      this.startTime = Date.now();
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className="swiperWrapper" ref={div => (this.wrapper = div)}>
+          <div ref={div => (this.background = div)} className="swiperBackground">
+            {this.props.background ? this.props.background : <span className="swiperDeleteText">Delete</span>}
+          </div>
+          <div ref={div => (this.listElement = div)} className="swiperListItem">
+            <div
+              ref={div => (this.listElement = div)}
+              onMouseDown={this.onDragStartMouse}
+              onTouchStart={this.onDragStartTouch}
+            >
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default SwipeableListItem
