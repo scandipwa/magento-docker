11a92751ed598e45c28440cfe80b3289e1a35809
Alfreds Genkins <alfreds@scandiweb.com>
2020-07-13 19:34:11 +0300

    Theme name (#106)
    * Updated scandipwa
    
    * Version 3.0 test.
    
    * Updated source, customizatio
    
    * Updated source
    
    * Updated attributes to remove shipping_weight from filterable variables.
    
    * Bumped ES version to 7.x
    
    * Updated theme, catalog, url-rewrite
    
    * Updated theme
    
    * Fixex "ERROR: <path> is empty!". Added variable theme name support via "SCANDIPWA_THEME" environment variable.
    
    * Mounted .application to frontend container
    
    * removed base-theme file
    
    * Updated composer to latest versions.
    
    Co-authored-by: Alfreds Genkins <alfreds.genkins@Users-MacBook-Pro.local>

--- deploy/start.sh
+++ deploy/start.sh
@@ -62,14 +62,14 @@ function pwa_theme_install {
   echo "${blue}${bold}Register PWA theme in Magento${normal}"
 
   # Theme setup
-  magento scandipwa:theme:bootstrap Scandiweb/pwa -n || true
+  magento scandipwa:theme:bootstrap "$SCANDIPWA_THEME" -n || true
   php bin/magento setup:upgrade
 
   if [ $? -eq 0 ]; then
     if [ "$frontend" != "1" ]
     then
       echo "${blue}${bold}Building PWA theme${normal}"
-      cd $BASEPATH/app/design/frontend/Scandiweb/pwa
+      cd $BASEPATH/app/design/frontend/$SCANDIPWA_THEME
       npm ci
       npm run build
       cd $BASEPATH
@@ -217,12 +217,12 @@ function magento_redis_config {
       -n
 
   # Elasticsearch5 as a search engine
-  echo "${blue}${bold}Setting Elasticsearch5 as a search engine${normal}"
-  php bin/magento config:set catalog/search/engine elasticsearch5
+  echo "${blue}${bold}Setting Elasticsearch7 as a search engine${normal}"
+  php bin/magento config:set catalog/search/engine elasticsearch7
 
   # elasticsearch container as a host name
   echo "${blue}${bold}Setting elasticsearch as a host name for Elasticsearch5${normal}"
-  php bin/magento config:set catalog/search/elasticsearch5_server_hostname elasticsearch
+  php bin/magento config:set catalog/search/elasticsearch7_server_hostname elasticsearch
   php bin/magento cache:enable
 }
 
